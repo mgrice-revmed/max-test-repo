0805f04b401b607c219d17ce31cb09958c4953f4
mgrice-revmed <mgrice@revmed.com>
2025-03-12 09:44:28 -0700

    Update test.py for fetch vs pull test

--- test.py
+++ test.py
@@ -4,3 +4,5 @@ print("Made some new changes that should be fetched")
 print("Made some changes to commit to remote browser")
 
 print("trying remote changes once more")
+
+print("changed on remote browser to test fetch vs pull")
